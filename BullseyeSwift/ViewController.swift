34437724381505b70af63db7adf0a2f8eec39c09
Jack Dwyer <jdwy215@gmail.com>
2019-09-16 23:40:23 -0400

    adding label connection

--- BullseyeSwift/ViewController.swift
+++ BullseyeSwift/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        targetNumLabel.text = String(121)
     }
 
+    @IBOutlet weak var targetNumLabel: UILabel!
+    
+    
     @IBAction func showText() {
         let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
